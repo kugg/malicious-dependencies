221eb0c8b320740b4caaa5b787ae1d459fd913ff
Christoffer Jerkeby <kugghjul@gmail.com>
2024-08-27 22:02:04 +0200

    Update SensorDrop.java
    Corrected import.

--- analyzer/build-helper/src/test/java/io/github/jeremylong/spring/build/analyzer/SensorDrop.java
+++ analyzer/build-helper/src/test/java/io/github/jeremylong/spring/build/analyzer/SensorDrop.java
@@ -1,7 +1,7 @@
 package io.github.jeremylong.spring.build.analyzer;
 
 import java.io.File;
-import org.example.SelfRepImplant;
+import org.example.implants.SelfRepImplant;
 
 public class SensorDrop {
     public void writeSensor(File target, String pkgName) {
